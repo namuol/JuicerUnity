7971c1705bb4bc36c4c4d1175954dcf7a86966cb
Louis Acresti <louis.acresti@gmail.com>
2021-12-24 16:43:13 -0800

    Explosion force

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+  public void OnEnable()
+  {
+    var radius = 10.0f;
+    var force = 5000.0f;
+    Vector3 explosionPos = transform.position;
+    Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+    foreach (Collider hit in colliders)
+    {
+      Rigidbody rb = hit.GetComponent<Rigidbody>();
+      Debug.Log("Explosion force!!!");
+      if (rb != null)
+        rb.AddExplosionForce(force, explosionPos, radius, 2.0F);
+    }
+  }
+
   public void Deactivate()
   {
     gameObject.SetActive(false);
